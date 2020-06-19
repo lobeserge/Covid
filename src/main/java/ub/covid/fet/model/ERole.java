1921b3373f6b0b549db58d381f66d80795062fd6
lobeserge <lobeserge100@gmail.com>
2020-06-19 10:38:22 +0100

    covidapp beta version

---
+++ src/main/java/ub/covid/fet/model/ERole.java
@@ -0,0 +1,6 @@
+package ub.covid.fet.model;
+
+public enum ERole {
+	ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/test/java/ub/covid/fet/FetApplicationTests.java
@@ -0,0 +1,13 @@
+package ub.covid.fet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
