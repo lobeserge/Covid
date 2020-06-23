45ed9c3169b0949aa40138ea823d320efbe77047
lobeserge <lobeserge100@gmail.com>
2020-06-23 18:55:14 +0100

    updated user table

--- src/main/java/ub/covid/fet/model/User.java
+++ src/main/java/ub/covid/fet/model/User.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 @Entity
-@Table(	name = "users",
+@Table(	name = "user_details",
 		uniqueConstraints = {
 			@UniqueConstraint(columnNames = "username"),
 			@UniqueConstraint(columnNames = "email")
